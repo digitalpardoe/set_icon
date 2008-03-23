312b8140561ef1bf05e2569b5c861fce64e81fb5
Alex Pardoe <digital.pardoe@gmail.com>
2008-03-23 13:51:55 +0000

    Initial project import.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Set Icon
+//
+//  Created by Alex on 23/03/2008.
+//  Copyright digital:pardoe 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
